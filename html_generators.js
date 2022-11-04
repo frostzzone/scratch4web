ea8c4729af88ae06009bf141bc218aac8221423a
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2022-11-04 08:06:18 -0700

    Update html_generators.js

--- html_generators.js
+++ html_generators.js
@@ -40,9 +40,12 @@ HtmlGenerator['baseframe'] = function(block) {
 HtmlGenerator['styler'] = function(block) {
   var statements_head = HtmlGenerator.statementToCode(block, 'head');
   // TODO: Assemble JavaScript into code variable.
-  var code = `<style>`;
-  +  statements_head 
-  + `</style>\n`
+  var code = `
+  <style> 
+   ${statements_head}
+  </style>
+  
+  `;
   return code;
 };
 
